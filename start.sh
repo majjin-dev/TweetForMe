7628efbd760496985138257bc28847fda8f7b42a
david <majjin.ov52t@simplelogin.co>
2022-05-01 15:01:46 -0500

    Run via docker in heroku

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+/app/tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1055 &
+/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=heroku-app
+echo Tailscale started
+cd tweetsforsats
+HTTP_PROXY=http://localhost:8055/ gunicorn tweetsforsats.wsgi
