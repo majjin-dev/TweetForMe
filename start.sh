a729f9d9fdd43a9d9928c621a4e88d6294fcca0a
david <majjin.ov52t@simplelogin.co>
2022-05-05 21:43:54 -0500

    Removed heroku stuff

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# TODO: Figure out permission denied
-/app/tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1055 &
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=heroku-app
-echo Tailscale started
-cd tweetsforsats
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-HTTP_PROXY=http://localhost:1055/ gunicorn tweetsforsats.wsgi
